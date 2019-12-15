fef83f19d540d665cd818806f1be0f5e5a3dba33
Derek Williams <souldishwasher@gmail.com>
2019-12-14 19:30:53 -0600

    adding excel upload functionality

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import flask_excel as excel
+import pandas as pd
+import sqlite3
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+""" app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) """
+
+conn = sqlite3.connect(':memory:')
+cur = conn.cursor()
 
 
-class Todo(db.Model):
+""" class Todo(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<Task %r>' % self.id """
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
+    conn = sqlite3.connect(':memory:')
+    cur = conn.cursor()
+    """ if request.method == 'POST':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
 
@@ -32,7 +40,15 @@ def index():
             return 'There was an issue'
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return render_template('index.html', tasks=tasks) """
+    if request.method == 'POST':
+        f = request.files['file']
+        data_xls = pd.read_excel(f)
+        data_xls.to_sql(name='tbl', con=conn)
+        return render_template('index.html', table=data_xls.to_html())
+    else:
+        cur.execute('SELECT * FROM sqlite_temp_master')
+        return render_template('index.html', table=cur)
 
 
 if __name__ == "__main__":
